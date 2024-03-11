053a91598d2276f6495912c1b60f1234433b87e2
Бігняк Олександр <gdsasha2000@gmail.com>
2024-03-11 03:47:27 +0200

    add test

---
+++ test/image_comparison_test.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:solid_test/feature/image_comparison/cubit/image_comparison_cubit.dart';
+import 'package:solid_test/feature/image_comparison/model/compare_images_params.dart';
+
+void main() {
+  test('Comparing identical images should return 0% difference', () async {
+    final imageData1 =
+        await File('test/resources/test_image_1.png').readAsBytes();
+    final imageData2 =
+        await File('test/resources/test_image_1.png').readAsBytes();
+    final result = await compareImagesInIsolate(
+        CompareImagesParams(imageData1, imageData2));
+
+    expect(result.differencePercent, 0.0);
+  });
+
+  test('Comparing images with different sizes returns an error state',
+      () async {
+    final imageData1 =
+        await File('test/resources/test_image_1.png').readAsBytes();
+    final imageData2 =
+        await File('test/resources/test_image_2.png').readAsBytes();
+    final result = await compareImagesInIsolate(
+        CompareImagesParams(imageData1, imageData2));
+
+    expect(result.error, true);
+  });
+}
